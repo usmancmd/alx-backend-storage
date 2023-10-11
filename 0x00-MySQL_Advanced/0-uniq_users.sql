9880e99fbf078f4d90a84865b1181611baf27c94
Usman Adepoju <adepojuusman465@gmail.com>
2023-10-11 18:58:24 +0100

    Add solution to task-0

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,10 @@
+-- creates a table users
+-- with id, email and name
+
+CREATE TABLE IF NOT EXISTS users (
+        id INT NOT NULL AUTO_INCREMENT,
+        email VARCHAR(255) NOT NULL,
+        name VARCHAR(255),
+        PRIMARY KEY (id),
+        UNIQUE (email)
+) ;
