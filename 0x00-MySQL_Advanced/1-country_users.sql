140549d2fc8599b3c5bc91692a49aa0744400f91
Usman Adepoju <adepojuusman465@gmail.com>
2023-10-11 19:30:45 +0100

    Add solution to task-1

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -2,9 +2,8 @@
 -- with id, email, name and country
 
 CREATE TABLE IF NOT EXISTS users (
-        id INT NOT NULL AUTO_INCREMENT,
+        id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
         email VARCHAR (255) NOT NULL UNIQUE,
-        name VARCHAR (255) NOT NULL,
-        country ENUM('US', 'CO', 'TN') DEFAULT 'US' NOT NULL,
-        PRIMARY KEY (id)
+        name VARCHAR (255),
+        country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US',
 );
