23c139bb5a69d9ee0cccd055989a888bc33f2876
Usman Adepoju <adepojuusman465@gmail.com>
2023-10-11 19:32:42 +0100

    Add solution to task-1

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -5,5 +5,5 @@ CREATE TABLE IF NOT EXISTS users (
         id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
         email VARCHAR (255) NOT NULL UNIQUE,
         name VARCHAR (255),
-        country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US',
+        country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
 );
