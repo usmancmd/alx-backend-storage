9415b31b821acfd9d59fcf48fffca698427a4052
Usman Adepoju <adepojuusman465@gmail.com>
2023-10-13 15:44:28 +0100

    Add solution to task-100

---
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -0,0 +1,17 @@
+-- creates a stored procedure ComputeAverageWeightedScoreForUser
+-- that computes and store the average weighted score for a student
+
+DELIMITER $$ ;
+CREATE PROCEDURE ComputeAverageWeightedScoreForUser(IN user_id INT)
+BEGIN
+    DECLARE weight_factor INT;
+    DECLARE number_x_weight_factor INT;
+    SELECT SUM(project_id) INTO weight_factor FROM corrections
+    WHERE corrections.user_id=user_id;
+    SELECT SUM(project_id * score) INTO number_x_weight_factor FROM corrections
+    WHERE corrections.user_id=user_id;
+    UPDATE users
+    SET average_score = number_x_weight_factor / weight_factor
+    WHERE users.id=user_id;
+END$$
+DELIMITER ; $$
