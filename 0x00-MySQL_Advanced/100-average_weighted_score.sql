7d812d0d9016a9af73b23c30c8eca7de0c094609
Usman Adepoju <adepojuusman465@gmail.com>
2023-10-13 15:54:05 +0100

    Add solution to task-12

--- 0x00-MySQL_Advanced/100-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -6,12 +6,21 @@ CREATE PROCEDURE ComputeAverageWeightedScoreForUser(IN user_id INT)
 BEGIN
     DECLARE weight_factor INT;
     DECLARE number_x_weight_factor INT;
+
     SELECT SUM(project_id) INTO weight_factor FROM corrections
     WHERE corrections.user_id=user_id;
+
     SELECT SUM(project_id * score) INTO number_x_weight_factor FROM corrections
     WHERE corrections.user_id=user_id;
-    UPDATE users
-    SET average_score = number_x_weight_factor / weight_factor
-    WHERE users.id=user_id;
+
+    IF number_x_weight_factor = 0 THEN
+        UPDATE users
+        SET average_score = 0
+        WHERE users.id=user_id;
+    ELSE
+        UPDATE users
+        SET average_score = number_x_weight_factor / weight_factor
+        WHERE users.id=user_id;
+    END IF;
 END$$
 DELIMITER ; $$
