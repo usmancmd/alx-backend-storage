fcbbd8b553d0f6e5daefd36afd85d4aa3df11929
Usman Adepoju <adepojuusman465@gmail.com>
2023-10-20 16:59:19 +0100

    Add methods to the class cache

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -23,4 +23,16 @@ class Cache():
         self._redis.set(key, data)
         return key
 
-        
+    def get(self, key: str, fn: Callable = None) -> Union[str, bytes, int, float, None]:
+        data = self._redis.get(key)
+        if data is None:
+            return None
+        if fn is not None:
+            return fn(data)
+        return data
+
+    def get_str(self, key: str) -> Union[str, None]:
+        return self.get(key, fn=lambda d: d.decode("utf-8"))
+
+    def get_int(self, key: str) -> Union[int, None]:
+        return self.get(key, fn=int)        
