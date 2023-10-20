32660f33f47a879f5582467914d6a03f706d5199
Usman Adepoju <adepojuusman465@gmail.com>
2023-10-20 11:31:38 +0100

    Add solution to task-10

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Changes all topics of a school document based on the name"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """Changes all topics of a school document"""
+    return mongo_collection.update_many(
+            {"name": name},
+            {"$set": {"topics": topics}}
+            ) 
