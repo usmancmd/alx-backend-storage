dcf17e986c6fc45266b06ab4cd36daa861a63d99
Usman Adepoju <adepojuusman465@gmail.com>
2023-10-20 10:15:08 +0100

    Add solution to task-9

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""inserts a new document in a collection based on kwargs"""
+
+
+def insert_school(mongo_collection, **kwargs):
+    """"inserts a new document in a collection"""
+    return mongo_collection.insert(kwargs).inserted_id
